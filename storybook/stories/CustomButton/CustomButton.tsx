2911691b859d0637ffb060c1585dab6efaf5c9d3
Tak Yu Chan <frankychantakyu@yahoo.com.hk>
2021-08-23 13:51:20 +1200

    support react native vector icons

--- storybook/stories/CustomButton/CustomButton.tsx
+++ storybook/stories/CustomButton/CustomButton.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
-import {TouchableHighlight, View} from 'react-native';
+import {TouchableHighlight} from 'react-native';
+import FontAwesome from 'react-native-vector-icons/FontAwesome';
 // import Svg, {Path, Line, Polyline} from 'react-native-svg';
 
 interface Props {
@@ -10,6 +11,7 @@ interface Props {
 export const CustomButton = (props: Props) => {
   return (
     <TouchableHighlight onPress={props.onPress}>
+      <FontAwesome name="deafness" size={50} />
       {/* <Svg
         height="100"
         width="100"
@@ -22,22 +24,6 @@ export const CustomButton = (props: Props) => {
         <Line x1="7" y1="7" x2="17" y2="17" />
         <Polyline points="17 8 17 17 8 17" />
       </Svg> */}
-
-      {/* <svg
-        xmlns="http://www.w3.org/2000/svg"
-        class="icon icon-tabler icon-tabler-arrow-down-right"
-        width="24"
-        height="24"
-        viewBox="0 0 24 24"
-        stroke-width="2"
-        stroke="currentColor"
-        fill="none"
-        stroke-linecap="round"
-        stroke-linejoin="round">
-        <path stroke="none" d="M0 0h24v24H0z" fill="none"></path>
-        <line x1="7" y1="7" x2="17" y2="17"></line>
-        <polyline points="17 8 17 17 8 17"></polyline>
-      </svg> */}
     </TouchableHighlight>
   );
 };
